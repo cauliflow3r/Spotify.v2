408e212a2adf7b56d7770ee9a50664a23b443102
Neilom <nil.porvani@gmail.com>
2023-06-02 17:29:01 +0600

    up

--- src/pages/Account.jsx
+++ src/pages/Account.jsx
@@ -1,6 +1,5 @@
-import React, { useContext } from "react";
+import React from "react";
 import { useAuth } from "../context/AuthContextProvider";
-import classes from "../style/Main.module.css";
 import { useNavigate } from "react-router-dom";
 import logoSpotify from "../assets/Spotify_Logo_CMYK_Black.png";
 import change from "../style/Login.module.css";
@@ -8,7 +7,6 @@ import FormLayout from "../layouts/FormLayout/FormLayout";
 
 const Account = () => {
   const {
-    currentUser,
     changePassword,
     setCurrentPassword,
     setNewPassword,
@@ -17,9 +15,8 @@ const Account = () => {
     newPassword,
     confirmPassword,
     refreshToken,
+    loading,
   } = useAuth();
-
-  const { loading } = useContext();
 
   const navigate = useNavigate();
 
@@ -35,17 +32,17 @@ const Account = () => {
 
   return (
     <FormLayout>
-      <div className={classes.wrapper}>
-        <div className={classes.imgBox}>
+      <div className={change.wrapper}>
+        <div className={change.imgBox}>
           <img src={logoSpotify} alt="logo" />
         </div>
         <hr />
-        <div className={classes.textBox}>
+        <div className={change.textBox}>
           <h1>Change password</h1>
         </div>
-        <div className={classes.mainForm}>
-          <div className={classes.fromP}>
-            <p>Email address or username</p>
+        <div className={change.mainForm}>
+          <div className={change.fromP}>
+            <p>Current password</p>
           </div>
           <div className={change.inpurblock}>
             <input
@@ -54,59 +51,42 @@ const Account = () => {
               value={currentPassword}
               onChange={(e) => setCurrentPassword(e.target.value)}
             />
-            {loading ? (
-              <button onClick={handleChangePassword}>Save</button>
-            ) : (
-              <button
-                className={classes.btnLoad}
-                onClick={handleChangePassword}
-              >
-                <div className={classes.wave}>
-                  <div className={classes.ball}></div>
-                  <div className={classes.ball}></div>
-                  <div className={classes.ball}></div>
-                  <div className={classes.ball}></div>
-                  <div className={classes.ball}></div>
-                </div>
-              </button>
-            )}
-          </div>
-          <div className={classes.support}>
-            <span>If you still need help, contact Spotify Support</span>
-          </div>
-        </div>
-
-        <div className={classes.container}>
-          <div className={classes.contentWrapper}>
-            <h2>ACCOUNT</h2>
-            <div className={classes.infoContainer}>
-              <div className={classes.email}>
-                <span>User email </span>
-                <p> &nbsp; : &nbsp;{currentUser}</p>
-              </div>
+            <div className={change.fromP}>
+              <p>New password</p>
             </div>
-            <div className={classes.ChangeBlock}>
-              <p>Change Passwor : &nbsp;</p>
-              <div className={classes.inpurblock}>
-                <input
-                  type="text"
-                  placeholder="current "
-                  value={currentPassword}
-                  onChange={(e) => setCurrentPassword(e.target.value)}
-                />
-                <input
-                  type="text"
-                  placeholder="new"
-                  value={newPassword}
-                  onChange={(e) => setNewPassword(e.target.value)}
-                />
+            <div className={change.inpurblock}>
+              <input
+                type="text"
+                placeholder="new"
+                value={newPassword}
+                onChange={(e) => setNewPassword(e.target.value)}
+              />
+              <div className={change.fromP}>
+                <p>New current password</p>
+              </div>
+              <div className={change.inpurblock}>
                 <input
                   type="text"
                   placeholder="confirm "
                   value={confirmPassword}
                   onChange={(e) => setConfirmPassword(e.target.value)}
                 />
-                <button onClick={handleChangePassword}>change</button>
+                {loading ? (
+                  <button onClick={handleChangePassword}>Save</button>
+                ) : (
+                  <button
+                    className={change.btnLoad}
+                    onClick={handleChangePassword}
+                  >
+                    <div className={change.wave}>
+                      <div className={change.ball}></div>
+                      <div className={change.ball}></div>
+                      <div className={change.ball}></div>
+                      <div className={change.ball}></div>
+                      <div className={change.ball}></div>
+                    </div>
+                  </button>
+                )}
               </div>
             </div>
           </div>
